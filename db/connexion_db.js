a599dc487c4e81d9cf78ed3a164c8717fc1f93fc
Alex Kamano <pk86onf@eduvaud.ch>
2025-11-13 12:12:54 +0100

    add a file data base

---
+++ db/connexion_db.js
@@ -0,0 +1,32 @@
+const db = {
+
+    connectToDB: async () => {
+        try {
+            const connection = await mysql.createConnection({
+                host: "localhost",
+                user: "root",
+                password: "",
+                port: 3306,
+                database: "cyno_client", //database name
+            });
+            console.log("Connected to DB");
+            return connection;
+        } catch (error) {
+            console.error("Error connexion DaaBase", error);
+            throw error;
+        }
+
+    },
+
+    disconnectFromDatabase: async (connection) => {
+        try {
+            await connection.end();
+            console.log('Déconnexion de la base de données réussie');
+        } catch (error) {
+            console.error('Erreur lors de la déconnexion de la base de données :', error);
+            throw error;
+        }
+    }
+}
+
+export { db }
