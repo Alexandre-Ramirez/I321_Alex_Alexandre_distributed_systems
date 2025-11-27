1145cda38c1cc9f88b6186dc441655b3a0a74d5c
ps78ppi <alexandre.ramirez@eduvaud.ch>
2025-11-27 11:02:14 +0100

    rename file + route pizzaOfTheMoment

--- routes/pizzaofthemomentRoute.js
+++ routes/pizzaofthemomentRoute.js
@@ -1,1 +1,10 @@
-//
+// routes/pizzaOfTheMomentRoute.js
+
+const express = require('express');
+const router = express.Router();
+const {fetchAllPizzaOfTheMoments} = require("../controllers/pizzaOfTheMomentController");
+
+// Getter pizza du moment
+router.get('/', fetchAllPizzaOfTheMoments);
+
+module.exports = router;
